cb2eda12bedc9bd999b5b20cdca83b7765da7b4f
Rafael Cassaro Costa <rafaelcassacosta@hotmail.com>
2025-07-05 17:41:00 -0300

    fix: portfolio techs size mobile res

--- mocks/portfolioCards.js
+++ mocks/portfolioCards.js
@@ -5,12 +5,12 @@ const cards = [
     cardGithub: "https://github.com/rafaelcassaro/learn-next",
     cardText: "<span class='text-bold'>Projeto:</span> Painel financeiro de faturas. <br> Curso do site oficial do Next.js sobre App Router onde aprendi, através da construção de uma aplicação web full-stack, conceitos de Styling, Optimizations, Routing, Data Fetching entre outros.",
     cardTechImgs: `
-      <img class='technologies__img card-tech-img' src='./imgs/techs/html-logo.png' alt='HTML Icon'>
-      <img class='technologies__img card-tech-img' src='./imgs/techs/css3-logo.png' alt='CSS Icon'>
-      <img class='technologies__img card-tech-img' src='./imgs/techs/typescript-logo.png' alt='TS Icon'>
-      <img class='technologies__img card-tech-img' src='./imgs/techs/react-logo.png' alt='React Icon'>
-      <img class='technologies__img card-tech-img' src='./imgs/techs/nextjs-logo.png' alt='Next.js Icon'>
-      <img class='technologies__img card-tech-img' src='./imgs/techs/tailwind-logo.png' alt='Tailwind Icon'>
+      <img class='portfolio_technologies__img card-tech-img' src='./imgs/techs/html-logo.png' alt='HTML Icon'>
+      <img class='portfolio_technologies__img card-tech-img' src='./imgs/techs/css3-logo.png' alt='CSS Icon'>
+      <img class='portfolio_technologies__img card-tech-img' src='./imgs/techs/typescript-logo.png' alt='TS Icon'>
+      <img class='portfolio_technologies__img card-tech-img' src='./imgs/techs/react-logo.png' alt='React Icon'>
+      <img class='portfolio_technologies__img card-tech-img' src='./imgs/techs/nextjs-logo.png' alt='Next.js Icon'>
+      <img class='portfolio_technologies__img card-tech-img' src='./imgs/techs/tailwind-logo.png' alt='Tailwind Icon'>
     `
   },
   {
@@ -19,12 +19,12 @@ const cards = [
     cardGithub: "https://github.com/rafaelcassaro/search-github-repos",
     cardText: "<span class='text-bold'>Projeto:</span> Procura de repositórios no GitHub. <br> Busca repositórios de um usuário no GitHub. O projeto foi feito com shallow routing e useParams, pois a API do GitHub possui uma quantidade limitada de requisições.",
     cardTechImgs: `
-      <img class='technologies__img card-tech-img' src='./imgs/techs/html-logo.png' alt='HTML Icon'>
-      <img class='technologies__img card-tech-img' src='./imgs/techs/css3-logo.png' alt='CSS Icon'>
-      <img class='technologies__img card-tech-img' src='./imgs/techs/typescript-logo.png' alt='TS Icon'>
-      <img class='technologies__img card-tech-img' src='./imgs/techs/react-logo.png' alt='React Icon'>
-      <img class='technologies__img card-tech-img' src='./imgs/techs/nextjs-logo.png' alt='Next.js Icon'>
-      <img class='technologies__img card-tech-img' src='./imgs/techs/tailwind-logo.png' alt='Tailwind Icon'>
+      <img class='portfolio_technologies__img card-tech-img' src='./imgs/techs/html-logo.png' alt='HTML Icon'>
+      <img class='portfolio_technologies__img card-tech-img' src='./imgs/techs/css3-logo.png' alt='CSS Icon'>
+      <img class='portfolio_technologies__img card-tech-img' src='./imgs/techs/typescript-logo.png' alt='TS Icon'>
+      <img class='portfolio_technologies__img card-tech-img' src='./imgs/techs/react-logo.png' alt='React Icon'>
+      <img class='portfolio_technologies__img card-tech-img' src='./imgs/techs/nextjs-logo.png' alt='Next.js Icon'>
+      <img class='portfolio_technologies__img card-tech-img' src='./imgs/techs/tailwind-logo.png' alt='Tailwind Icon'>
     `
   },
   {
@@ -33,10 +33,10 @@ const cards = [
     cardGithub: "https://github.com/rafaelcassaro/react-globals-state",
     cardText: "<span class='text-bold'>Projeto:</span> E-commerce em fase de desenvolvimento inicial. <br> Gerenciei estados globais do react utilizando, context APi para evitar 'prop drilling', useReducer para controle de estado e useMemo para otimização e desempenho.",
     cardTechImgs: `
-      <img class='technologies__img card-tech-img' src='./imgs/techs/html-logo.png' alt='HTML Icon'>
-      <img class='technologies__img card-tech-img' src='./imgs/techs/css3-logo.png' alt='CSS Icon'>
-      <img class='technologies__img card-tech-img' src='./imgs/techs/js-logo2.png' alt='JS Icon'>
-      <img class='technologies__img card-tech-img' src='./imgs/techs/react-logo.png' alt='React Icon'>
+      <img class='portfolio_technologies__img card-tech-img' src='./imgs/techs/html-logo.png' alt='HTML Icon'>
+      <img class='portfolio_technologies__img card-tech-img' src='./imgs/techs/css3-logo.png' alt='CSS Icon'>
+      <img class='portfolio_technologies__img card-tech-img' src='./imgs/techs/js-logo2.png' alt='JS Icon'>
+      <img class='portfolio_technologies__img card-tech-img' src='./imgs/techs/react-logo.png' alt='React Icon'>
     `
   },
   {
@@ -45,10 +45,10 @@ const cards = [
     cardGithub: "https://github.com/rafaelcassaro/countries-api-react",
     cardText: "<span class='text-bold'>Projeto:</span> Consumo de uma API com React. <br> Consumi uma API pública de informações de países, e através de React Hooks, mostrei na tela e salvei em cards as informações.",
     cardTechImgs: `
-      <img class='technologies__img card-tech-img' src='./imgs/techs/html-logo.png' alt='HTML Icon'>
-      <img class='technologies__img card-tech-img' src='./imgs/techs/css3-logo.png' alt='CSS Icon'>
-      <img class='technologies__img card-tech-img' src='./imgs/techs/js-logo2.png' alt='JS Icon'>
-      <img class='technologies__img card-tech-img' src='./imgs/techs/react-logo.png' alt='React Icon'>
+      <img class='portfolio_technologies__img card-tech-img' src='./imgs/techs/html-logo.png' alt='HTML Icon'>
+      <img class='portfolio_technologies__img card-tech-img' src='./imgs/techs/css3-logo.png' alt='CSS Icon'>
+      <img class='portfolio_technologies__img card-tech-img' src='./imgs/techs/js-logo2.png' alt='JS Icon'>
+      <img class='portfolio_technologies__img card-tech-img' src='./imgs/techs/react-logo.png' alt='React Icon'>
     `
   },
   {
@@ -57,8 +57,8 @@ const cards = [
     cardGithub: "https://github.com/rafaelcassaro/Alurabook-web",
     cardText: "<span class='text-bold'>Projeto:</span> Landing page de loja de venda de livros. <br> Apliquei layouts responsivos com media queries, e utilizei HTML e CSS baseado em um protótipo do Figma para construção do site.",
     cardTechImgs: `
-      <img class='technologies__img card-tech-img' src='./imgs/techs/html-logo.png' alt='HTML Icon'>
-      <img class='technologies__img card-tech-img' src='./imgs/techs/css3-logo.png' alt='CSS Icon'>
+      <img class='portfolio_technologies__img card-tech-img' src='./imgs/techs/html-logo.png' alt='HTML Icon'>
+      <img class='portfolio_technologies__img card-tech-img' src='./imgs/techs/css3-logo.png' alt='CSS Icon'>
     `
   },
   {
@@ -67,9 +67,9 @@ const cards = [
     cardGithub: "https://github.com/rafaelcassaro/fokus/tree/main",
     cardText: "<span class='text-bold'>Projeto:</span> Site para estudo com método Pomodoro. <br> Estudo sobre manipulação de DOM com JS e localStorage.",
     cardTechImgs: `
-      <img class='technologies__img card-tech-img' src='./imgs/techs/html-logo.png' alt='HTML Icon'>
-      <img class='technologies__img card-tech-img' src='./imgs/techs/css3-logo.png' alt='CSS Icon'>
-      <img class='technologies__img card-tech-img' src='./imgs/techs/js-logo2.png' alt='JS Icon'>
+      <img class='portfolio_technologies__img card-tech-img' src='./imgs/techs/html-logo.png' alt='HTML Icon'>
+      <img class='portfolio_technologies__img card-tech-img' src='./imgs/techs/css3-logo.png' alt='CSS Icon'>
+      <img class='portfolio_technologies__img card-tech-img' src='./imgs/techs/js-logo2.png' alt='JS Icon'>
     `
   },
   {
@@ -78,9 +78,9 @@ const cards = [
     cardGithub: "https://github.com/rafaelcassaro/Alurabooks-list/tree/main",
     cardText: "<span class='text-bold'>Projeto:</span> Opções de livros em uma loja. <br> Estudo sobre manipulação de arrays e filtros de objetos na página.",
     cardTechImgs: `
-      <img class='technologies__img card-tech-img' src='./imgs/techs/html-logo.png' alt='HTML Icon'>
-      <img class='technologies__img card-tech-img' src='./imgs/techs/css3-logo.png' alt='CSS Icon'>
-      <img class='technologies__img card-tech-img' src='./imgs/techs/js-logo2.png' alt='JS Icon'>
+      <img class='portfolio_technologies__img card-tech-img' src='./imgs/techs/html-logo.png' alt='HTML Icon'>
+      <img class='portfolio_technologies__img card-tech-img' src='./imgs/techs/css3-logo.png' alt='CSS Icon'>
+      <img class='portfolio_technologies__img card-tech-img' src='./imgs/techs/js-logo2.png' alt='JS Icon'>
     `
   },
   {
@@ -89,8 +89,8 @@ const cards = [
     cardGithub: "https://github.com/rafaelcassaro/OptimusTech",
     cardText: "<span class='text-bold'>Projeto:</span> Site fictício para cliente. <br> Cliente fictício fornecido pelo desafio Alura 7DaysOfCode. Site feito seguindo template do Figma.",
     cardTechImgs: `
-      <img class='technologies__img card-tech-img' src='./imgs/techs/html-logo.png' alt='HTML Icon'>
-      <img class='technologies__img card-tech-img' src='./imgs/techs/css3-logo.png' alt='CSS Icon'>
+      <img class='portfolio_technologies__img card-tech-img' src='./imgs/techs/html-logo.png' alt='HTML Icon'>
+      <img class='portfolio_technologies__img card-tech-img' src='./imgs/techs/css3-logo.png' alt='CSS Icon'>
     `
   }
 ];
